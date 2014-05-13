0b4a32ff279f0cad5dd9088b944a8b5127fc371b
Kazato Sugimoto <uiureo@gmail.com>
2014-05-14 01:02:54 +0900

    Refactor

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var exec = require('child_process').exec
   , path = require('path')
   , fs = require('fs')
   , tmp = require('tmp')
+  , resizeIfRetina = require('./lib/resize')
   , request = require('request')
 
 function upload(idFilePath, imagePath) {
@@ -25,44 +26,8 @@ tmp.file(function (err, imagePath) {
   exec('screencapture -i ' + imagePath, function (err) {
     if (err) throw err
 
-    function sips(property, callback) {
-      return exec('sips -g ' + property + ' ' + imagePath + ' | awk \'/:/ {print $2}\'', function (err, stdout) {
-        callback(Number(stdout))
-      })
-    }
-
-    function dpi(callback) {
-      sips('dpiWidth', function (width) {
-        sips('dpiHeight', function (height) {
-          callback({ width: width, height: height })
-        })
-      })
-    }
-
-    function pixel(callback) {
-      sips('pixelWidth', function (width) {
-        sips('pixelHeight', function (height) {
-          callback({ width: width, height: height })
-        })
-      })
-    }
-
-    dpi(function (dpi) {
-      if (dpi.width > 72.0 && dpi.height > 72.0) {
-        pixel(function (pixel) {
-          var width = pixel.width * 72.0 / dpi.width
-            , height = pixel.height * 72.0 / dpi.height
-
-          exec('sips -s dpiWidth 72 -s dpiHeight 72 -z ' + height + ' ' + width + ' ' + imagePath, function () {
-            upload(idFilePath, imagePath)
-          })
-        })
-
-        return
-      }
-
+    resizeIfRetina(imagePath, function () {
       upload(idFilePath, imagePath)
     })
-
   })
 })
