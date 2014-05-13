81664b0a9f264260f51f2a43d3615033da2194b4
Kazato Sugimoto <uiureo@gmail.com>
2014-05-13 23:29:29 +0900

    Use tmp

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var exec = require('child_process').exec
   , path = require('path')
   , fs = require('fs')
+  , tmp = require('tmp')
   , request = require('request')
 
 var idFilePath = path.join(process.env.HOME, 'Library/Gyazo/id')
 
-exec('screencapture -i a.png', function () {
-  var requestStream =
-    request.post('http://gyazo.com/upload.cgi', function (err, res, body) {
-      exec(['echo', body , '| pbcopy'].join(' '))
-      exec('open ' + body)
-    })
+tmp.file(function (err, imagePath) {
+  if (err) throw err
 
-  var form = requestStream.form()
-  form.append('id', fs.readFileSync(idFilePath))
-  form.append('imagedata', fs.createReadStream(path.join(__dirname, 'a.png')))
+  exec('screencapture -i ' + imagePath, function () {
+    var requestStream =
+      request.post('https://gyazo.com/upload.cgi', function (err, res, body) {
+        exec(['echo', body , '| pbcopy'].join(' '))
+        exec('open ' + body)
+      })
+
+    var form = requestStream.form()
+    form.append('id', fs.readFileSync(idFilePath))
+    form.append('imagedata', fs.createReadStream(imagePath))
+  })
 })
